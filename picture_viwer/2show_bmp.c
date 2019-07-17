5b74c97bed6e8ef5b927e6f67281002df517cba5
yoling <yoling@qq.com>
2019-07-17 16:49:26 +0800

    show unsigned

--- picture_viwer/2show_bmp.c
+++ picture_viwer/2show_bmp.c
@@ -11,6 +11,7 @@
 
 int main(void)
 {
+    //打开LCD屏幕文件
     int fd = open("/dev/fb0",O_RDWR);
     if(fd == -1)
     {
@@ -31,40 +32,50 @@ int main(void)
         perror("mmap");
         return -2;
     }
+	char *bmp_name = "a.bmp";
+	
+	//1、打开bmp文件400*300
+	FILE *bmp_fp = fopen(bmp_name, "r");
+	if(bmp_fp ==NULL)
+	{
+		perror("fopen bmp failed!");
+		printf("fopen %s failed!\n", bmp_name);	
+		return -1;
+	}
+	unsigned char bmp_buf[500*340*3];
 
-    //w = 500   h = 340
-    int fd_bmp = open("a.bmp", O_RDONLY);
-    if(fd_bmp == -1)
-    {
-        perror("open");
-        return -1;
-    }
+	//	2、跳过54个字节文件头
+	fseek(bmp_fp, 54, SEEK_SET);
 
-    lseek(fd_bmp,  54,  SEEK_SET);
-    
-    unsigned char buf[3];
-    memset(buf, 0, sizeof(buf));
-    int w = 0;
-    int h = 0;
+    	//	3、读取bmp图片的像素点数据
+	fread(bmp_buf, 500*340*3, 1, bmp_fp);
 
-    int lcd_buf[340*500];
+    	//	4、关闭bmp文件
+	fclose(bmp_fp);
+
+    unsigned int lcd_buf[500*340];//400*300*4
     memset(lcd_buf, 0, sizeof(lcd_buf));
 
+    int w = 0, h = 0;
     for(h = 0; h < 340; h++)
     {
         for(w = 0; w < 500; w++)
         {
-            unsigned int ret = read(fd_bmp, buf, sizeof(buf));
-            //printf("%d,%d,%d,%d\n",ret,buf[0],buf[1],buf[2]);
-            
-            //lcd_buf[w+h*500] = buf[0]  | buf[1] << 8 | buf[2] << 16;
+            /*
+            lcd_buf[w+h*500] = bmp_buf[3*w+500*3*h+0] ;
+			lcd_buf[w+h*500] |= bmp_buf[3*w+500*3*h+1]<<8;
+			lcd_buf[w+h*500] |= bmp_buf[3*w+500*3*h+2]<<16;
+             */
+            lcd_buf[w+h*500] = bmp_buf[3*w+500*3*h+0] | bmp_buf[3*w+500*3*h+1]<<8 | bmp_buf[3*w+500*3*h+2]<<16;
+
+           //read(fd_bmp, buf, sizeof(buf));
             //1   2     3    4
             //      红  绿  蓝
             //*(memp + w + h*var.xres) = 0x000000ff;
-            *(memp + w + h*var.xres) = buf[0] << 0  | buf[1] << 8 | buf[2] << 16;
+            //*(memp + w + h*var.xres) = buf[0] << 0  | buf[1] << 8 | buf[2] << 16;
         }
     }
-/*
+
     for(h = 0; h < 340; h++)
     {
         for(w = 0; w < 500; w++)
@@ -72,23 +83,15 @@ int main(void)
             *(memp + w + h*var.xres) = lcd_buf[w+500*h];
         }
     }
- */
-
 /*
     int i = 0;
-    for(i = 0; i < var.xres * var.yres; i++)
+    for(i = 0;i < var.xres*var.yres; i++)
     {
-        read(fd_bmp, buf, 3);
-
-        *(memp+i) = buf[0] | buf[1] << 4 | buf[2] << 8;
-    }
- */
-
+        *(memp+i) = 0xff0000;
+    } 
+*/
 
     munmap(memp,var.xres*var.yres*var.bits_per_pixel/8);
-    close(fd_bmp);
     close(fd);
-
-
     return 0;
 }
